644ce560e33995142fab048aa6e701a203cdb7cb
tayyabtahir870@gmail.com <tayyabtahir870@gmail.com>
2022-10-26 18:08:57 +0500

    safuu

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Components/Layouts/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Layouts/Header.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import {BsTwitter} from 'react-icons/bs'
+import {FaDiscord} from 'react-icons/fa'
+import {FaTelegramPlane} from 'react-icons/fa'
+import {SiMedium} from 'react-icons/si'
+
+function Header() {
+  return (
+    <div>
+         <div className="header bg-black  p-3  ">
+        <nav class="navbar navbar-expand-lg   ">
+          <div class="container-fluid ">
+            <a class="navbar-brand" href="/">
+            <button className='header-button'><b>SAFUU</b></button>
+            </a>
+
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div className="">
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item"></li>
+                  <li class="nav-item">
+                    <a className="nav-link mt-3 " to='/technology'>
+                      < BsTwitter color='white' size={30}/>
+                    </a> 
+                   
+                  </li>{" "} 
+                  <li class="nav-item">
+                    <a class="nav-link mt-3" to='/community'>
+                      <FaDiscord color='white' size={30}/>
+                    </a>
+                  </li>{" "} 
+                  <li class="nav-item">
+                    <a class="nav-link mt-3" to='/aboutpage'>
+                     <FaTelegramPlane color='white' size={30}/>
+                    </a>
+                  </li>{" "} 
+                  <li class="nav-item">
+                    <a class="nav-link mt-3" to="/blog">
+                    <SiMedium color='white' size={30}/>
+                    </a>
+                  </li>{" "} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <li class="nav-item">
+                    <a class="nav-link " to="/build">
+                    <button className='header-button2'><b>SAFUU</b></button>
+                    </a>
+                  </li>{" "} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <li class="nav-item">
+                    <a class="nav-link" to="/contact">
+                    <button className='header-button1'><b>Connect Wallet</b></button>
+                    </a>
+                  </li>{" "} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <hr className='colorline' />
+    </div>
+    
+  )
+}
+
+export default Header
